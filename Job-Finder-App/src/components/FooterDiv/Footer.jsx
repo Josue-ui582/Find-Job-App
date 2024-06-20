045ac37228aab5204dbb819d56512328362fe4de
Josué Jésugnon HOUNHUOI <hounhouijosue1@gmail.com>
2024-06-20 13:53:49 +0200

    update

---
+++ Job-Finder-App/src/components/FooterDiv/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>Footer</div>
+    </div>
+  )
+}
+
+export default Footer
